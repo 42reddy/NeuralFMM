af9c94fca539111c207c4ca2d27ad264964cb6fc
42reddy <110433102+42reddy@users.noreply.github.com>
2026-09-15 21:36:58 +0530

    changed les->fmm

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ MAX_VAL_SAMPLES = None  # None = use all test structures
 # ----------------------------------------------------------------------
 # Model
 # ----------------------------------------------------------------------
-ARCHITECTURE = "les"  # "fmm" or "les"
+ARCHITECTURE = "fmm"  # "fmm" or "les"
 
 SHARED_HYPERPARAMS = dict(
     hidden_dim=256,
