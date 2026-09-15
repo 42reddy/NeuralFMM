e8f4ed85adbdee082275c88e380c41d3ed36ee03
42reddy <110433102+42reddy@users.noreply.github.com>
2026-09-15 19:28:30 +0530

    changed n epochs

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ CHECKPOINT_DIR = f"checkpoints/{DATASET}-{ARCHITECTURE}"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 TRAIN_CONFIG = TrainConfig(
-    epochs=10,
+    epochs=200,
     batch_size=64,
     lr=1e-3,
     energy_weight=1.0,
